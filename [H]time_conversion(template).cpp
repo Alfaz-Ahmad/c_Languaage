de5a6a795ec86e75b92a48a9011b29176cf9956e
Alfaz-Ahmad <120320595+Alfaz-Ahmad@users.noreply.github.com>
2023-05-26 22:43:17 +0530

    Add files via upload

---
+++ [H]time_conversion(template).cpp
@@ -0,0 +1,137 @@
+#include <assert.h>
+#include <ctype.h>
+#include <limits.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char* readline();
+
+/*
+ * Complete the 'timeConversion' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts STRING s as parameter.
+ */
+
+/*
+ * To return the string from the function, you should either do static allocation or dynamic allocation
+ *
+ * For example,
+ * char* return_string_using_static_allocation() {
+ *     static char s[] = "static allocation of string";
+ *
+ *     return s;
+ * }
+ *
+ * char* return_string_using_dynamic_allocation() {
+ *     char* s = malloc(100 * sizeof(char));
+ *
+ *     s = "dynamic allocation of string";
+ *
+ *     return s;
+ * }
+ *
+ */
+char* timeConversion(char* s) {
+    int i, sum=0;
+    if(*(s+8)=='A'){
+        for(i=0;i<2;i++){
+            sum += (int)*(s+i)-48;
+        }
+        printf("%d",sum);
+        if(sum == 3){
+            for(i=0;i<2;i++){
+                *(s+i) = '0';
+            }
+        }
+    }
+    else if(*(s+8) == 'P'){
+        for(i=0;i<2;i++){
+            sum += (*(s+i)-48)*(pow(10,2-i-1));
+        }
+        if(sum == 12){
+            *(s+8) = '\0';
+            return s;
+        }
+        sum += 12;
+        for(i=1;i>=0;i--){
+            *(s+i) = sum%10 + 48;
+            sum /= 10;
+        }
+    }
+    *(s+8) = '\0';
+    return s;
+}
+
+int main()
+{
+    FILE* fptr = fopen(getenv("OUTPUT_PATH"), "w");
+
+    char* s = readline();
+
+    char* result = timeConversion(s);
+
+    fprintf(fptr, "%s\n", result);
+
+    fclose(fptr);
+
+    return 0;
+}
+
+char* readline() {
+    size_t alloc_length = 1024;
+    size_t data_length = 0;
+
+    char* data = malloc(alloc_length);
+
+    while (true) {
+        char* cursor = data + data_length;
+        char* line = fgets(cursor, alloc_length - data_length, stdin);
+
+        if (!line) {
+            break;
+        }
+
+        data_length += strlen(cursor);
+
+        if (data_length < alloc_length - 1 || data[data_length - 1] == '\n') {
+            break;
+        }
+
+        alloc_length <<= 1;
+
+        data = realloc(data, alloc_length);
+
+        if (!data) {
+            data = '\0';
+
+            break;
+        }
+    }
+
+    if (data[data_length - 1] == '\n') {
+        data[data_length - 1] = '\0';
+
+        data = realloc(data, data_length);
+
+        if (!data) {
+            data = '\0';
+        }
+    } else {
+        data = realloc(data, data_length + 1);
+
+        if (!data) {
+            data = '\0';
+        } else {
+            data[data_length] = '\0';
+        }
+    }
+
+    return data;
+}
+
