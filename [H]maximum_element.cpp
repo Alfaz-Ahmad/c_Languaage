8404e4fa0e42b88407a56b6943288c04ca649c0f
Alfaz-Ahmad <120320595+Alfaz-Ahmad@users.noreply.github.com>
2023-07-03 22:05:10 +0530

    Add files via upload

---
+++ [H]maximum_element.cpp
@@ -0,0 +1,217 @@
+#include <assert.h>
+#include <ctype.h>
+#include <limits.h>
+#include <math.h>
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+char* readline();
+char* ltrim(char*);
+char* rtrim(char*);
+
+int parse_int(char*);
+
+/*
+ * Complete the 'getMax' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts STRING_ARRAY operations as parameter.
+ */
+
+/*
+ * To return the integer array from the function, you should:
+ *     - Store the size of the array to be returned in the result_count variable
+ *     - Allocate the array statically or dynamically
+ *
+ * For example,
+ * int* return_integer_array_using_static_allocation(int* result_count) {
+ *     *result_count = 5;
+ *
+ *     static int a[5] = {1, 2, 3, 4, 5};
+ *
+ *     return a;
+ * }
+ *
+ * int* return_integer_array_using_dynamic_allocation(int* result_count) {
+ *     *result_count = 5;
+ *
+ *     int *a = malloc(5 * sizeof(int));
+ *
+ *     for (int i = 0; i < 5; i++) {
+ *         *(a + i) = i + 1;
+ *     }
+ *
+ *     return a;
+ * }
+ *
+ */
+int* getMax(int operations_count, char** operations, int* result_count) {
+    long i, j=0, k=0, x, c = 0, t;
+    int* r = (int*)malloc(operations_count*sizeof(int));
+    long* s = (long*)malloc(operations_count*sizeof(long));
+    long* m = (long*)malloc(operations_count*sizeof(long));
+    for(i=0;i<operations_count;i++){
+        if(strlen((*(operations+i)))==1){
+            long max = -1;
+            //printf(">>>%d\n",*(*(operations+i))-48);
+            if(*(*(operations+i))-48 == 2){
+                j--;
+            }else{
+                for(t=0;t<j;t++){
+                    //printf("=%d\n",*(s+t));
+                    if(max < *(s+t)){
+                        max = *(s+t);
+                        //printf("%d\t",*(s+t));
+                    }
+                }
+                printf("->%d %d\n",max,j);
+                *(r+(c++)) = max;
+            }
+        }else{
+            long sum = 0;
+            for(x=strlen((*(operations+i)))-1;x>=2;x--){
+                sum += (*(*(operations+i)+x)-48)*(pow(10,(strlen((*(operations+i)))-x)-1));
+            }
+            *(s+(j++)) = sum;
+            //printf(">%d\t%d\n",sum,*(s+(j-1)));
+        }
+    }
+    *(result_count) = c;
+    return r;
+}
+
+int main()
+{
+    FILE* fptr = fopen(getenv("OUTPUT_PATH"), "w");
+
+    int n = parse_int(ltrim(rtrim(readline())));
+
+    char** ops = malloc(n * sizeof(char*));
+
+    for (int i = 0; i < n; i++) {
+        char* ops_item = readline();
+
+        *(ops + i) = ops_item;
+    }
+
+    int res_count;
+    int* res = getMax(n, ops, &res_count);
+
+    for (int i = 0; i < res_count; i++) {
+        fprintf(fptr, "%d", *(res + i));
+
+        if (i != res_count - 1) {
+            fprintf(fptr, "\n");
+        }
+    }
+
+    fprintf(fptr, "\n");
+
+    fclose(fptr);
+
+    return 0;
+}
+
+char* readline() {
+    size_t alloc_length = 1024;
+    size_t data_length = 0;
+
+    char* data = malloc(alloc_length);
+
+    while (true) {
+        char* cursor = data + data_length;
+        char* line = fgets(cursor, alloc_length - data_length, stdin);
+
+        if (!line) {
+            break;
+        }
+
+        data_length += strlen(cursor);
+
+        if (data_length < alloc_length - 1 || data[data_length - 1] == '\n') {
+            break;
+        }
+
+        alloc_length <<= 1;
+
+        data = realloc(data, alloc_length);
+
+        if (!data) {
+            data = '\0';
+
+            break;
+        }
+    }
+
+    if (data[data_length - 1] == '\n') {
+        data[data_length - 1] = '\0';
+
+        data = realloc(data, data_length);
+
+        if (!data) {
+            data = '\0';
+        }
+    } else {
+        data = realloc(data, data_length + 1);
+
+        if (!data) {
+            data = '\0';
+        } else {
+            data[data_length] = '\0';
+        }
+    }
+
+    return data;
+}
+
+char* ltrim(char* str) {
+    if (!str) {
+        return '\0';
+    }
+
+    if (!*str) {
+        return str;
+    }
+
+    while (*str != '\0' && isspace(*str)) {
+        str++;
+    }
+
+    return str;
+}
+
+char* rtrim(char* str) {
+    if (!str) {
+        return '\0';
+    }
+
+    if (!*str) {
+        return str;
+    }
+
+    char* end = str + strlen(str) - 1;
+
+    while (end >= str && isspace(*end)) {
+        end--;
+    }
+
+    *(end + 1) = '\0';
+
+    return str;
+}
+
+int parse_int(char* str) {
+    char* endptr;
+    int value = strtol(str, &endptr, 10);
+
+    if (endptr == str || *endptr != '\0') {
+        exit(EXIT_FAILURE);
+    }
+
+    return value;
+}
+
