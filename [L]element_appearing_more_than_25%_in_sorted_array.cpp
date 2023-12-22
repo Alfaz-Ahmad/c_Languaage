affaeb1810cc45391f2d8151e595f86f1905a9e3
Alfaz-Ahmad <120320595+Alfaz-Ahmad@users.noreply.github.com>
2023-12-23 00:46:25 +0530

    Add files via upload

---
+++ [L]element_appearing_more_than_25%_in_sorted_array.cpp
@@ -0,0 +1,19 @@
+int findSpecialInteger(int* arr, int arrSize) {
+    int threshold = (arrSize*25)/100;
+    int max = 0, i;
+    for(i=0;i<arrSize;i++){
+        if(max < *(arr+i)){
+            max = *(arr+i);
+        }
+    }
+    int* hash = (int*)calloc(max+1, sizeof(int));
+    for(i=0;i<arrSize;i++){
+        (*(hash+*(arr+i)))++;
+    }
+    for(i=0;i<arr;i++){
+        if(*(hash+*(arr+i)) > threshold){
+            return *(arr+i);
+        }
+    }
+    assert(false);
+}
